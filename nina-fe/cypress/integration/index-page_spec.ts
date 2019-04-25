3580a737446b6a2c79ca67ff638a4e0f84bf00a6
Adekanmi Ademiiju <maneptha@gmail.com>
2019-04-25 10:12:13 +0200

    FE e2e test for index page - wip.

--- nina-fe/cypress/integration/index-page_spec.ts
+++ nina-fe/cypress/integration/index-page_spec.ts
@@ -1,6 +1,22 @@
 import { SITE_TITLE } from "../../src/constants";
+import * as inputReg from "../../src/__tests__/utils-sign-up";
+import { CreateUserInput } from "../../src/apollo-generated";
+
+const TEST_USER: CreateUserInput = {
+  username: "john",
+  email: "a@b.com",
+  password: "123456"
+};
 
 describe("index page", function() {
+  // beforeEach(() => {
+  //   cy.checkoutSession();
+  // });
+
+  // afterEach(() => {
+  //   cy.closeSession();
+  // });
+
   it("loads successfully", function() {
     /**
      * Given a user is on the home page
@@ -11,5 +27,19 @@ describe("index page", function() {
      * Then user should see the title
      */
     cy.title().should("eq", SITE_TITLE);
+
+    /**
+     * When user fills and submits the form
+     */
+    cy.getByLabelText(inputReg.usernameInputReg).type(TEST_USER.username);
+    cy.getByLabelText(inputReg.emailInputReg).type(TEST_USER.email);
+    cy.getByLabelText(inputReg.passwordInputReg).type(TEST_USER.password);
+    cy.getByLabelText(inputReg.repeatPasswordInputReg).type(TEST_USER.password);
+    cy.getByText(inputReg.submitBtnTextReg).click();
+
+    /**
+     * Then user should be redirected to the welcome page
+     */
+    cy.title().should("contain", "Welcome");
   });
 });
--- nina-fe/cypress/support/commands.ts
+++ nina-fe/cypress/support/commands.ts
@@ -1,2 +1,15 @@
 import "cypress-testing-library/add-commands";
 import "cypress-file-upload";
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       *
+       */
+      checkoutSession: () => Chainable<Promise<void>>;
+
+      closeSession: () => Chainable<Promise<void>>;
+    }
+  }
+}
