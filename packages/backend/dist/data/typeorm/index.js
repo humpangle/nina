9dac615b8b979caa94b45942346465f13dfe21ab
Adekanmi Ademiiju <maneptha@gmail.com>
2019-04-27 09:33:44 +0200

    BE removed dist folder from git.

--- packages/backend/dist/data/typeorm/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var user_1 = require("./user");
-exports.dbCreateUser = user_1.createUser;
-exports.dbLogin = user_1.login;
-exports.dbGetUserById = user_1.getUserById;
-exports.dbGetUserByEmail = user_1.getUserByEmail;
-// getCredentialByUserId as dbGetCredentialByUserId,
-exports.dbUpdateCredential = user_1.updateCredential;
