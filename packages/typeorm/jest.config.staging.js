617f43a2c6dff164b6bde85b33f24ce19f3d5f80
Adekanmi Ademiiju <maneptha@gmail.com>
2019-05-02 22:52:44 +0200

    Ty jest config staging coverage.

--- packages/typeorm/jest.config.staging.js
+++ packages/typeorm/jest.config.staging.js
@@ -1,9 +1,14 @@
 module.exports = {
   testEnvironment: "node",
-  testPathIgnorePatterns: [
-    "<rootDir>/node_modules/",
-    "<rootDir>/src/.+?db.test.ts"
+  coverageDirectory: "./coverage",
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "<rootDir>/dist/migration/",
+    "<rootDir>/dist/winston-logger.js",
+    "<rootDir>/dist/make-ormconfig.js"
   ],
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
   testRegex: "dist/__tests__/.+?\\.test\\.js$",
   globals: {
     __PATH_PREFIX__: ""
